fe39a32893ae683016488307774ab6e4b7c62060
NeilRaiden <NeilRaidenLLC@gmail.com>
2025-09-24 14:30:10 -0400

    update CSS styles

--- layout.js
+++ layout.js
@@ -22,21 +22,25 @@ const footerHtml = [
 const styleSetupHtml = [
 '<div class="grid-container">',
 '	<div>Text font size:</div>',
-'	<div><button class="pgSetup" onclick="changeTxtFontSize(-1)">-</button></div>',
+'	<div><button class="pgSetup" onclick="changeTxtFontSize(-1)"> - </button></div>',
 '	<div id="textSize">14pt</div>',
-'	<div><button class="pgSetup" onclick="changeTxtFontSize(1)">+</button></div>',
-'	<div>Headings font size:</div>',
-'	<div><button class="pgSetup" onclick="changeHeadFontSize(-1)">-</button></div>',
-'	<div id="headSize">28pt</div>',
-'	<div><button class="pgSetup" onclick="changeHeadFontSize(1)">+</button></div>',
+'	<div><button class="pgSetup" onclick="changeTxtFontSize(1)"> + </button></div>',
+//'	<div>Headings font size:</div>',
+//'	<div><button class="pgSetup" onclick="changeHeadFontSize(-1)"> - </button></div>',
+//'	<div id="headSize">28pt</div>',
+//'	<div><button class="pgSetup" onclick="changeHeadFontSize(1)"> + </button></div>',
+'	<div>IPA symbols font size:</div>',
+'	<div><button class="pgSetup" onclick="changeIPAFontSize(-1)"> - </button></div>',
+'	<div id="ipaSize">12pt</div>',
+'	<div><button class="pgSetup" onclick="changeIPAFontSize(1)"> + </button></div>',
 '	<div>Line spacing:</div>',
-'	<div><button class="pgSetup" onclick="changeLineHeight(-0.1)">-</button></div>',
-'	<div id="lineHigh">1.1</div>',
-'	<div><button class="pgSetup" onclick="changeLineHeight(0.1)">+</button></div>',
+'	<div><button class="pgSetup" onclick="changeLineHeight(-0.1)"> - </button></div>',
+'	<div id="lineHigh">1.4</div>',
+'	<div><button class="pgSetup" onclick="changeLineHeight(0.1)"> + </button></div>',
 '	<div>Word spacing:</div>',
-'	<div><button class="pgSetup" onclick="changeWordSpace(-1)">-</button></div>',
+'	<div><button class="pgSetup" onclick="changeWordSpace(-1)"> - </button></div>',
 '	<div id="wordSpc">1px</div>',
-'	<div><button class="pgSetup" onclick="changeWordSpace(1)">+</button></div>',
+'	<div><button class="pgSetup" onclick="changeWordSpace(1)"> + </button></div>',
 '</div>' ];
 
 // ---------- functions ----------
@@ -55,11 +59,12 @@ function insertNavBar() {
 }
 
 // this function is called from  webPageSetup() - see below
-function insertFontPickers() {
-	let code = '<div class="grid-container" style="grid-template-columns: auto auto;">\n';
+function insertFontPickers(path) {
+//	let code = '<div id="fontPick" class="modal">\n';
+	let	code = '<div class="grid-container" style="grid-template-columns: auto auto;">\n';
 
 // font picker for ordinary text
-		code += '	<div>Text font:</div>\n';
+		code += '	<div><a href="' + path + 'fonts.html">Text font</a>:</div>\n';
 		code += '	<div><select id="textFontSelect" name="Select text font" onchange="changeTextFont(this)">\n';
 
 	for (let i in fontList) {
@@ -70,7 +75,7 @@ function insertFontPickers() {
 	code += '	</select></div>\n';
 
 // font picker for headings only (h1, h2, ..., h6)
-	code += '	<div>Header font:</div>\n';
+	code += '	<div><a href="' + path + 'fonts.html">Header font</a>:</div>\n';
 	code += '	<div><select id="headFontSelect" name="Select header font" onchange="changeHeadFont(this)">\n';
 
 	for (let i in fontList) {
@@ -81,27 +86,33 @@ function insertFontPickers() {
 
 	code += '	</select></div>\n';
 	code += '</div> <!-- endof: grid-container -->\n';
+//	code += '</div>\n';
+	
 	document.write(code);
 }
 
 function fontsCompareTable() {
-	let style = "";
-	let code = '<table><tr><th>Font Name</th><th>Digits</th><th>alphabet</th><th>ALPHABET</th><th>Icons</th></tr>\n';
+	let tdstyle = "";
+	let code = '<table><tr><th>Font Name</th><th>Digits</th><th>agy</th><th>alphabet</th><th>ALPHABET</th><th>Icons</th></tr>\n';
 	for (let i in fontList) {
-		style = '<td style="font-family:' + fontList[i].name + ';">';
+		tdstyle = '<td style="font-family:' + fontList[i].name + ', blank;">';
 		code += '<tr><td>' + fontList[i].name + '</td>';
-		code += style + '0123456789</td>';
-		code += style + 'abcdefghijklmnopqrstuvwxyz</td>';
-		code += style + 'ABCDEFGHIJKLMNOPQRSTUVWXYZ</td>';
-		code += style + '&#x2b06; &#x1f51d; &#x2630; &#x2699;</td></tr>\n';
+		code += tdstyle + '0123456789</td>';
+		code += tdstyle + 'agy</td>';
+		code += tdstyle + 'abcdefghijklmnopqrstuvwxyz</td>';
+		code += tdstyle + 'ABCDEFGHIJKLMNOPQRSTUVWXYZ</td>';
+		code += tdstyle + '&#x2b06; &#x1f51d; &#x2630; &#x2699;</td></tr>\n';
 	}
 	code += '</table>\n';
 	document.write(code);
 }
 
-function webPageSetup() {
+function webPageSetup(path) {
+/*	let code = '<div style="text-align:left;"><button style="padding:0px 12px;font-size:24px;"  onclick="toggleBlock(\'pageSetup\')"><span title="Styles preferences">&#x2699;</span></button></div>\n';
+	document.write(code);
+	*/
 	insertHTML(styleSetupHtml);
-	insertFontPickers();
+	insertFontPickers(path);
 }
 
 /* Toggle navigation menu - the hamburger menu on top bar */
@@ -111,6 +122,16 @@ function toggleBlock(blockName) {
     x.style.display = "none";
   } else {
     x.style.display = "block";
+  }
+}
+
+/* Toggle navigation menu - the hamburger menu on top bar */
+function flexBlock(blockName) {
+  let x = document.getElementById(blockName);
+  if (x.style.display === "flex") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "flex";
   }
 }
 
@@ -161,11 +182,19 @@ function changeTxtFontSize(val) {
 	}
 }
 
-function changeHeadFontSize(val) {
-	if( ( val < 0 && headFontSize > 8 ) || ( val > 0 && headFontSize < 50 ) ) {
-		headFontSize += val;
-		r.style.setProperty('--fontSizeHead', headFontSize + 'pt');
-		document.getElementById('headSize').innerHTML = headFontSize + "pt";
+//function changeHeadFontSize(val) {
+//	if( ( val < 0 && headFontSize > 8 ) || ( val > 0 && headFontSize < 50 ) ) {
+//		headFontSize += val;
+//		r.style.setProperty('--fontSizeHead', headFontSize + 'pt');
+//		document.getElementById('headSize').innerHTML = headFontSize + "pt";
+//	}
+//}
+
+function changeIPAFontSize(val) {
+	if( ( val < 0 && ipaFontSize > 8 ) || ( val > 0 && ipaFontSize < 50 ) ) {
+		ipaFontSize += val;
+		r.style.setProperty('--fontSizeIPA', ipaFontSize + 'pt');
+		document.getElementById('ipaSize').innerHTML = ipaFontSize + "pt";
 	}
 }
 
@@ -197,9 +226,10 @@ function changeWordSpace(val) {
 
 // global JS variables 
 // - some of these variables have their corresponding counterpart in the ":root" CSS global variables
-var textFontSize  =  14; 	// text   font-size (default 12pt)
-var headFontSize  =  28; 	// header font-size (default 24pt)
-var lineHeight    =  1.1;
+var textFontSize  =  14; 	// text     font-size (default 14pt)
+//var headFontSize  =  28; 	// header   font-size (default 24pt)
+var ipaFontSize   =  12; 	// IPA tags font-size (default 12pt) - font-family: NotoSans
+var lineHeight    =  1.4;
 var wordSpace     =  1;
 var fontFamilyText ='Now';
 var fontFamilyHead ='Dangrek';
